b2346d39a37c957bf9589086c8bbdc04769403d3
muazzem <muazzem2468@gmail.com>
2021-03-28 00:43:56 +0600

    first commit

---
+++ src/app/pages/layout/layout.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  NbAccordionModule,
+  NbButtonModule,
+  NbCardModule,
+  NbListModule,
+  NbRouteTabsetModule,
+  NbStepperModule,
+  NbTabsetModule, NbUserModule,
+} from '@nebular/theme';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { LayoutRoutingModule } from './layout-routing.module';
+import { LayoutComponent } from './layout.component';
+import { InfiniteListComponent } from './infinite-list/infinite-list.component';
+import { NewsPostComponent } from './infinite-list/news-post/news-post.component';
+import { NewsPostPlaceholderComponent } from './infinite-list/news-post-placeholder/news-post-placeholder.component';
+import { NewsService } from './news.service';
+
+@NgModule({
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    ThemeModule,
+    NbTabsetModule,
+    NbRouteTabsetModule,
+    NbStepperModule,
+    NbCardModule,
+    NbButtonModule,
+    NbListModule,
+    NbAccordionModule,
+    NbUserModule,
+    LayoutRoutingModule,
+  ],
+  declarations: [
+    LayoutComponent,
+    NewsPostPlaceholderComponent,
+    InfiniteListComponent,
+    NewsPostComponent,
+  ],
+  providers: [
+    NewsService,
+  ],
+})
+export class LayoutModule { }
---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,90 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'E-commerce',
+    icon: 'shopping-cart-outline',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'FEATURES',
+    group: true,
+  },
+  {
+    title: 'Layout',
+    icon: 'layout-outline',
+    children: [
+      {
+        title: 'Infinite List',
+        link: '/pages/layout/infinite-list',
+      },
+    ],
+  },
+  {
+    title: 'Forms',
+    icon: 'edit-2-outline',
+    children: [
+      {
+        title: 'Form Inputs',
+        link: '/pages/forms/inputs',
+      },
+      {
+        title: 'Form Layouts',
+        link: '/pages/forms/layouts',
+      },
+      {
+        title: 'Buttons',
+        link: '/pages/forms/buttons',
+      },
+      {
+        title: 'Datepicker',
+        link: '/pages/forms/datepicker',
+      },
+    ],
+  },
+  {
+    title: 'Modal & Overlays',
+    icon: 'browser-outline',
+    children: [
+      {
+        title: 'Dialog',
+        link: '/pages/modal-overlays/dialog',
+      },
+      {
+        title: 'Window',
+        link: '/pages/modal-overlays/window',
+      },
+      {
+        title: 'Popover',
+        link: '/pages/modal-overlays/popover',
+      },
+      {
+        title: 'Toastr',
+        link: '/pages/modal-overlays/toastr',
+      },
+      {
+        title: 'Tooltip',
+        link: '/pages/modal-overlays/tooltip',
+      },
+    ],
+  },
+  {
+    title: 'Charts',
+    icon: 'pie-chart-outline',
+    children: [
+      {
+        title: 'Echarts',
+        link: '/pages/charts/echarts',
+      },
+      {
+        title: 'Charts.js',
+        link: '/pages/charts/chartjs',
+      },
+      {
+        title: 'D3',
+        link: '/pages/charts/d3',
+      },
+    ],
+  },
+];
